093af1844b01a4f35e2cb8f83725278d297ee3cd
Henrique Bontempo <henrique.bontempo@cloudwalk.io>
2024-11-12 16:54:02 -0300

    refactor: Update form action and add new table row for actions

--- lib/sidekiq/belt/pro/force_batch_callback.rb
+++ lib/sidekiq/belt/pro/force_batch_callback.rb
@@ -12,20 +12,31 @@ module Sidekiq
             action_chars = action.chars
             action_button = action_chars[0].upcase + action_chars[1..].join
             <<~ERB
-              <form action="<%= root_path %>batches/<%= bid %>/force_callback/#{action}" method="post">
+              <form action="<%= root_path %>batches/<%= @batch.bid %>/force_callback/#{action}" method="post">
                 <%= csrf_tag %>
                 <input class="btn btn-danger" type="submit" name="#{action_name}" value="<%= t('#{action_button}') %>"
-                  data-confirm="Do you want to force the #{action} callback for batch <%= bid %>? <%= t('AreYouSure') %>" />
+                  data-confirm="Do you want to force the #{action} callback for batch <%= @batch.bid %>? <%= t('AreYouSure') %>" />
               </form>
             ERB
           end
 
           def self.registered(app)
             app.replace_content("/batches/:bid") do |content|
-              intermediary_position = content.index("<th>Complete</th>")
-              intermediary_position_2 = content[intermediary_position..].index("</tr>")
-              success_position = intermediary_position_2 + 5
-              content.insert(success_position, "<td>#{action_button('success')}</td>")
+              content.sub!(/(<\/tbody>)/) do |match|
+                <<-HTML
+                  <tr>
+                    <th><%= t("Force Action") %></th>
+                    <td>
+                      <div style="display: flex;">
+                        #{action_button('success')}
+                        #{action_button('complete')}
+                        #{action_button('death')}
+                      </div>
+                    </td>
+                  </tr>
+                  #{match}
+                HTML
+              end
             end
 
             app.post("/batches/:bid/force_callback/:action") do
